92ca7923f62da685ba0271f95a3ee88f87b39bcb
larry adams <ladams1776@yahoo.com>
2021-02-13 14:47:32 -0500

    Build out the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import { createBrowserHistory as createHistory } from 'history';
+import TopBar from './TopBar';
+import HistoricRatesBetweenCurrenciesPage from './HistoricRatesBetweenCurrenciesPage';
+import HistoricRatePage from './HistoricRatePage';
 import './App.css';
 
-function App() {
+const history = createHistory();
+
+const App = () => {
+  windoew.Chart.defaults.global.defaultFontFamily = `
+  -apple-system, BlinkMacSystemFont, "Segoe  UI", 
+  "Roboto", "Oxygen", "Ubuntu", "Cantarell", 
+  "Fira Sans", "Droid Sans", "Helvetica Neue", 
+  sans-serif`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <TopBar />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/historicrates" exact component={HistoricRatePage} />
+        <Route path="/historicrates2currencies" exact component={HistoricRatesBetweenCurrenciesPage} />
+
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
