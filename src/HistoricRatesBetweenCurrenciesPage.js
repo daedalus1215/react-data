0dbaafb0969955c38954fde58e268d851451954f
larry adams <ladams1776@yahoo.com>
2021-02-14 07:18:40 -0500

    Finish up the HistoricRatesBetweenCurrenciesPage

--- src/HistoricRatesBetweenCurrenciesPage.js
+++ src/HistoricRatesBetweenCurrenciesPage.js
@@ -10,13 +10,14 @@ import {
 } from "./requests";
 import { Line } from "react-chartjs-2";
 import { CURRENCIES } from "./exports";
+import { FormControl } from "react-bootstrap";
 
 const schema = yup.object({
     startDate: yup
         .string()
         .required("Start date is required")
         .matches(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/),
-        endDate: yup
+    endDate: yup
         .string()
         .required('End date is required')
         .matches(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/),
@@ -29,7 +30,7 @@ const HistoricRatesBetweenCurrenciesPage = () => {
 
     const handleSubmit = async evt => {
         const isValid = await schema.validate(evt);
-        if(!isValid) {
+        if (!isValid) {
             return;
         }
         const params = {
@@ -47,7 +48,7 @@ const HistoricRatesBetweenCurrenciesPage = () => {
                     data: Object.keys(rates).map(key => rates[key][evt.toCurrency]),
                     label: `${evt.fromCurrency} to ${evt.toCurrency}`,
                     borderColor: '#3e95cd',
-                    fill:false,
+                    fill: false,
                 },
             ],
         };
@@ -58,8 +59,96 @@ const HistoricRatesBetweenCurrenciesPage = () => {
         <div className="historic-rates-page">
             <h1 className="center">Historic Rates</h1>
             <Formik validationSchema={schema} onSubmit={handleSubmit}>
-                {/* //@TODO: left off here */}
+                {({
+                    handleSubmit,
+                    handleChange,
+                    handleBur,
+                    values,
+                    touched,
+                    isInvalid,
+                    errors
+                }) => (
+                    <Form noValidate noSubmit={handleSubmit}>
+                        <Form.Row>
+                            <Form.Group as={col} md="12" controlId="startDate">
+                                <Form.Label>Start Date</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="startDate"
+                                    placeHolder="YYYY-MM-DD"
+                                    value={values.startDate || ""}
+                                    onChange={handleSubmit}
+                                    isInvalid={touched.startDate && errors.startDate}
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.startDate}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md="12" controlId="endDate">
+                                <Form.Label>End Date</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="endDate"
+                                    placeholder="YYYY-MM-DD"
+                                    value={values.endDate || ""}
+                                    onChange={handleChange}
+                                    isInvalid={touched.endDate && errors.endDate}
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.endDate}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md="12" controlId="formCurrency">
+                                <Form.Label>Form Currency</Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    placeholder="From Currency"
+                                    name="fromCurrency"
+                                    onChange={handleChange}
+                                    value={values.fromCurrency || ""}
+                                    isInvalid={touched.fromCurrency && errors.fromCurrency}>
+                                    <option>Select</option>
+                                    {
+                                        CURRENCIES.filter(c => c != values.toCurrency)
+                                            .map(c => (<option key={c} value={c}>{c}</option>))
+                                    }
+                                </Form.Control>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.fromCurrency}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md="12" controlId="currency">
+                                <Form.Label>To Currency</Form.Label>
+                                <Form.Control
+                                    placeHolder="To Currency"
+                                    name="toCurrency"
+                                    onChange={handleChange}
+                                    value={values.toCurrency || ""}
+                                    isInvalid={touched.toCurrency && errors.toCurrency}>
+                                    <option>Select</option>
+                                    {
+                                        CURRENCIES.filter(c => c != values.fromCurrency)
+                                            .map(c => (<option key={c} value={c}></option>))
+                                    }
+                                </Form.Control>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.toCurrency}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Form.Row>
+                        <Button type="submit" style={{ marginRight: "10px" }}>Search</Button>
+                    </Form>
+                )}
             </Formik>
+            <br />
+            <div style={{ height: "400px", width: "90vw", margin: "0 auto" }}>
+                <Line data={data} />
+            </div>
         </div>
     );
 };
+
+export default HistoricRatesBetweenCurrenciesPage;
