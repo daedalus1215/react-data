3e850fbac53e9e207346c6073e2aec1c7a0b63d1
larry adams <ladams1776@yahoo.com>
2021-02-15 15:39:53 -0500

    Create the TopBar

---
+++ src/TopBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { withRouter } from "react-router-dom";
+function TopBar({ location }) {
+    const { pathname } = location;
+    return (
+        <Navbar bg="primary" expand="lg" variant="dark">
+            <Navbar.Brand href="#home">Currenc Converter App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/" active={pathname == "/"}>
+                        Home
+          </Nav.Link>
+                    <Nav.Link
+                        href="/historicrates"
+                        active={pathname.includes("/historicrates")}
+                    >
+                        Historic Rates
+          </Nav.Link>
+                    <Nav.Link
+                        href="/historicrates2currencies"
+                        active={pathname.includes("/historicrates2currencies")}
+                    >
+                        Historic Rates Between 2 Currencies
+          </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+export default withRouter(TopBar);
