b7f4ccf5e083ddca318cf03b21dc66d4b7e5938e
larry adams <ladams1776@yahoo.com>
2021-02-15 15:39:19 -0500

    Add Form to HistoricRatesPage

--- src/HistoricRatesPage.js
+++ src/HistoricRatesPage.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Formik } from 'formik';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import * as yup from 'yup';
-import { getHistoricRates } from './requests';
-import { Line } from 'react-chartjs-2';
-import { CURRENCIES } from './exports';
-import './HistoricRatesPage.css'
-
+import React from "react";
+import { Formik } from "formik";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import * as yup from "yup";
+import "./HistoricRatesPage.css";
+import { getHistoricRates } from "./requests";
+import { Line } from "react-chartjs-2";
+import { CURRENCIES } from "./exports";
 const schema = yup.object({
     startDate: yup
         .string()
@@ -18,13 +17,16 @@ const schema = yup.object({
         .string()
         .required("End date is required")
         .matches(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/),
-    currency: yup.string().required("Currency is required"),
+    currency: yup
+        .string()
+        .required("Currency is required"),
 });
 
-const HistoricRatesPage = async evt => {
-    const [data, setData] = useState({});
+function HistoricRatesPage() {
+    const [data, setData] = React.useState({});
 
     const handleSubmit = async evt => {
+        console.log('handleSubmit', evt)
         const isValid = await schema.validate(evt);
         if (!isValid) {
             return;
@@ -34,15 +36,15 @@ const HistoricRatesPage = async evt => {
             end_at: evt.endDate,
         };
         const response = await getHistoricRates(params);
+        console.log('HistoricRatesPage, ', response)
         const rates = response.data.rates;
         const lineGraphData = {
             labels: Object.keys(rates),
             datasets: [
                 {
-                    data: Object.keys(rates)
-                        .map.map(key => rates[key][evt.currency]),
+                    data: Object.keys(rates).map(key => rates[key][evt.currency]),
                     label: `EUR to ${evt.currency}`,
-                    borderColor: '#3e95cd',
+                    borderColor: "#3e95cd",
                     fill: false,
                 },
             ],
@@ -53,14 +55,84 @@ const HistoricRatesPage = async evt => {
     return (
         <div className="historic-rates-page">
             <h1 className="center">Historic Rates</h1>
-            <Formik validationSchema={schema} onSubmit={handleSubmit}>
+            <Formik validationSchema={schema} onSubmit={handleSubmit}
+                initialValues={{
+                    startDate: "2020-05-05",
+                    endDate: "2020-05-05",
+                    currency: ""
+                }}>
                 {({
-                    //@TODO: Left off here.
-                })}
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                    touched,
+                    isInvalid,
+                    errors,
+                }) => (
+                    <>
+                        {console.log('what are the values', values)}
+                        <Form noValidate onSubmit={handleSubmit}>
+                            <Form.Row>
+                                <Form.Group as={Col} md="12" controlId="startDate">
+                                    <Form.Label>Start Date</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="startDate"
+                                        placeholder="YYYY-MM-DD"
+                                        onChange={handleChange}
+                                        isInvalid={touched.startDate && errors.startDate}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.startDate}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="12" controlId="endDate">
+                                    <Form.Label>End Date</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="endDate"
+                                        placeholder="YYYY-MM-DD"
+                                        onChange={handleChange}
+                                        isInvalid={touched.endDate && errors.endDate}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.endDate}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="12" controlId="currency">
+                                    <Form.Label>Currency</Form.Label>
+                                    <Form.Control
+                                        as="select"
+                                        placeholder="Currency"
+                                        name="currency"
+                                        onChange={handleChange}
+                                        isInvalid={touched.currency && errors.currency}
+                                    >
+                                        <option>Select</option>
+                                        {CURRENCIES.map(c => (
+                                            <option key={c} value={c}>
+                                                {c}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.country}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Form.Row>
+                            <Button type="submit" style={{ marginRight: "10px" }}>
+                                Search
+            </Button>
+                        </Form>
+                    </>
+                )}
             </Formik>
+            <br />
+            <div style={{ height: "400px", width: "90vw", margin: "0 auto" }}>
+                <Line data={data} />
+            </div>
         </div>
     );
-};
-
-
+}
 export default HistoricRatesPage;
