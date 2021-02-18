cab39deb8da98fb7ce11ebee25d3c45e74224035
larry adams <ladams1776@yahoo.com>
2021-02-17 21:52:56 -0500

    Stub in a method to get bitcoin data from main proj

--- src/requests.js
+++ src/requests.js
@@ -6,9 +6,19 @@ export const getExchangeRate = () => {
     return axios.get(`${APIURL}/latest`);
 }
 
-export const getRateBetweenCurrencies = data => axios.get(`${APIURL}/history?${querystring.encode(data)}`);
+export const getRateBetweenCurrencies = data => axios
+    .get(`${APIURL}/history?${querystring.encode(data)}`);
 
-export const getHistoricRates = data => axios.get(`${APIURL}/history?${querystring.encode(data)}`);
+export const getHistoricRates = data => axios
+    .get(`${APIURL}/history?${querystring.encode(data)}`);
 
-export const getHistoricRatesBetweenCurrencies = data => axios.get(`${APIURL}/history?${querystring.encode(data)}`);
+export const getHistoricRatesBetweenCurrencies = data => axios
+    .get(`${APIURL}/history?${querystring.encode(data)}`);
 
+//@TODO: Clean this up
+export const getHistoricStuff = async (setHData) => await axios
+    .get(`http://localhost:8081/api/history/BTC`)
+    .then(async resp => {
+        setHData(await resp.data);
+    })
+    .catch(err => err);
